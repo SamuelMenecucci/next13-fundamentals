0dfc5b68ca10eb8b2499c931e24bedc31d4a1d23
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2024-05-30 14:19:08 -0300

    abstracting client components

---
+++ src/app/catalog/product/[id]/add-to-cart-button.tsx
@@ -0,0 +1,18 @@
+//To use Client Components, you can add the React "use client" directive at the top of a file, above your imports.
+//"use client" is used to declare a boundary between a Server and Client Component modules. This means that by defining a "use client" in a file, all other modules imported into it, including child components, are considered part of the client bundle.
+//DOC: https://react.dev/reference/rsc/use-client
+//I need to use the client side if the application requires any user iteration, where javascript will be used (to trigger a function when clicking a button, for example)
+"use client";
+
+import { useState } from "react";
+
+export function AddToCartButton() {
+  const [count, setCount] = useState(0);
+
+  function handleAdd() {
+    console.log("Added");
+    setCount((state) => state++);
+  }
+
+  return <button onClick={handleAdd}>Adicionar ao carrinho ({count})</button>;
+}
--- src/app/catalog/product/[id]/page.tsx
+++ src/app/catalog/product/[id]/page.tsx
@@ -5,7 +5,9 @@
 //"use client" is used to declare a boundary between a Server and Client Component modules. This means that by defining a "use client" in a file, all other modules imported into it, including child components, are considered part of the client bundle.
 //DOC: https://react.dev/reference/rsc/use-client
 //I need to use the client side if the application requires any user iteration, where javascript will be used (to trigger a function when clicking a button, for example)
-"use client";
+// "use client";
+
+import { AddToCartButton } from "./add-to-cart-button";
 
 interface ProductProps {
   params: {
@@ -13,17 +15,24 @@ interface ProductProps {
   };
 }
 
-export default function Product(props: ProductProps) {
+export default async function Product(props: ProductProps) {
+  const response = await fetch("https://api.github.com/users/samuelmenecucci");
+
+  const user = await response.json();
+
   console.log(props);
 
-  function handleAdd() {
-    console.log("Added");
-  }
+  // function handleAdd() {
+  //   console.log("Added");
+  // }
 
   return (
     <>
       <h1>Product: {props.params.id}</h1>
-      <button onClick={handleAdd}>Adicionar ao carrinho</button>
+      {/* <button onClick={handleAdd}>Adicionar ao carrinho</button> */}
+      {/* As I cannot use an asynchronous component when it is a client component, I can separate what is a client from what is a server. A server component can use a component that is a server component. Since my entire component here only needs to be a client component because of the button, I can separate the components that are client components from those that are server components. that is, I can leave the part that is a server component from that which is client component, this way I only load the javascript that is necessary.
+       */}
+      <AddToCartButton />
     </>
   );
 }
